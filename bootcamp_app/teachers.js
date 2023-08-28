d6db4ecf040562f7f9b12eb3a02701eb13a7be8f
sophia ojegba <roseysofia@yahoo.com>
2023-08-28 14:30:14 -0400

    using parameterized queries to secure entries to database

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,20 +6,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-//Name of Teachers That Assisted
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM assistance_requests
-JOIN teachers on teachers.id = teacher_id
-JOIN students on students.id = student_id
-JOIN cohorts on cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${process.argv[2] || 'JUL02'}'
-ORDER BY teachers.name
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM assistance_requests
+  JOIN teachers on teachers.id = teacher_id
+  JOIN students on students.id = student_id
+  JOIN cohorts on cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teachers.name
+  LIMIT $2
+`;
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.teacher}: ${row.cohort}`);
+    });
   })
-})
-.catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
